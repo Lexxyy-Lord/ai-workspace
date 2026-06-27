6daaf8aa677c65023f6cd74d9ca57c10eaf94c3b
Lixeyy <felixmahesa11@gmail.com>
2026-06-27 16:50:35 +0700

    feat(frontend): export static assets for single-port mode

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   reactStrictMode: true,
   poweredByHeader: false,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
+  images: {
+    unoptimized: true,
   },
 };
 
